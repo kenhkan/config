8df9889be97451a45a08e9cf0e5fac88e20e86d0
Kenneth Kan <ken@pixbi.com>
2015-01-06 09:29:32 -0500

    Don't point to Haskell Platform paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH /Users/kenhkan/.config/bin /Users/kenhkan/.cabal/bin /Users/kenhkan/Library/Haskell/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /opt/X11/bin
+set PATH /Users/kenhkan/.config/bin /Users/kenhkan/.cabal/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /opt/X11/bin
 setenv EDITOR vim
 
 
