fbdd78f84149814f2b0c2937fa316e0d20935ed1
Kenneth Kan <ken@pixbi.com>
2015-02-05 20:31:29 -0500

    Always update cabal

--- fish/functions/cinst.fish
+++ fish/functions/cinst.fish
@@ -1,4 +1,5 @@
 function cinst
+  cabal update
   cabal install --only-dependencies --enable-tests --enable-benchmarks --enable-library-profiling --jobs
   cabal configure --enable-profiling --enable-tests --enable-benchmarks
 end
