59dcdf21edff7383f8daee3f58a128eff38a5b8d
Kenneth Kan <kenhkan@gmail.com>
2015-12-18 22:47:46 -0500

    Show relative time and committer in log

--- fish/functions/glog.fish
+++ fish/functions/glog.fish
@@ -1,3 +1,3 @@
 function glog
-  git log --oneline --graph --decorate --all
+  git log --graph --all --abbrev-commit --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
 end
