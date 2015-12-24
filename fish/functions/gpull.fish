3494ca9f4ff9eaa2913e5ef0df10daae6c7cb63f
Kenneth Kan <kenhkan@gmail.com>
2015-12-24 13:06:32 -0500

    Push and pull first before tracking

--- fish/functions/gpull.fish
+++ fish/functions/gpull.fish
@@ -1,8 +1,8 @@
 function gpull
+  git pull --rebase $argv
+
   set argc (count $argv)
   if [ $argc = 2 ]
     git branch --set-upstream-to $argv[1]/$argv[2]
   end
-
-  git pull --rebase $argv
 end
