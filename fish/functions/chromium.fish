f0050cd6eee463622a9a8351777d2193b9f88f7f
Kenneth Kan <kenneth.kan@mheducation.com>
2016-09-28 17:08:55 -0400

    Log chromium log to file

--- fish/functions/chromium.fish
+++ fish/functions/chromium.fish
@@ -1,3 +1,3 @@
 function chromium
-  open -n -a /opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app --args --disable-web-security
+  open -n -a /opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app --args --disable-web-security --enable-logging --v=1
 end
