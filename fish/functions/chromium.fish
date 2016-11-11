e2e4243c5ca70103177dd51b2338d3ad0a3ca80f
Kenneth Kan <kenneth.kan@mheducation.com>
2016-11-11 09:50:26 -0500

    --user-data-dir is needed

--- fish/functions/chromium.fish
+++ fish/functions/chromium.fish
@@ -1,3 +1,3 @@
 function chromium
-  open -n -a /opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app --args --disable-web-security --enable-logging --v=1
+  open -n -a /opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app --args --disable-web-security --user-data-dir --enable-logging --v=1
 end
