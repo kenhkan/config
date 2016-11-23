14d576bf612e6f45353f0fb6a7fa9dc6af25e6e9
Kenneth Kan <kenhkan@gmail.com>
2016-11-22 23:09:12 -0500

    No X11

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH $HOME/.config/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /opt/X11/bin
+set PATH $HOME/.config/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin
 setenv EDITOR vim
 
 
