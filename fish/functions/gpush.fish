e51480d087c33b43c1535c192a3ecc45cb58f090
Kenneth Kan <kenhkan@gmail.com>
2016-12-04 17:35:20 -0500

    Always push with set upstream

--- fish/functions/gpush.fish
+++ fish/functions/gpush.fish
@@ -2,7 +2,7 @@ function gpush
   set argc (count $argv)
 
   if [ $argc = 2 ]
-    git branch --set-upstream-to $argv[1]/$argv[2]
+    git push --set-upstream  $argv[1] $argv[2]
   end
 
   git push
