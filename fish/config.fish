8d4cd52d396795d6efb49dfb683a3c7e56cde482
Kenneth Kan <kenhkan@gmail.com>
2017-08-10 10:18:09 -0400

    Add PostgreSQL command-lines

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH $HOME/.config/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin
+set PATH $HOME/.config/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /Applications/Postgres.app/Contents/Versions/latest/bin
 setenv EDITOR vim
 
 
