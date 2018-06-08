fa24c1fdebc6593fd7aaa9bcfea18984bd9d0b88
Kenneth Kan <kenhkan@gmail.com>
2018-06-07 23:50:56 -0400

    Set upstream by default for git push

--- fish/functions/gpush.fish
+++ fish/functions/gpush.fish
@@ -1,9 +1,3 @@
 function gpush
-  set argc (count $argv)
-
-  if [ $argc = 2 ]
-    git push --set-upstream  $argv[1] $argv[2]
-  end
-
-  git push
+  git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)
 end
