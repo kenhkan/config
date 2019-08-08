2ce284f879f91b268570197d1d03f334c1dc6fa5
Ken Kan <kenhkan@gmail.com>
2019-08-08 11:21:16 -0400

    Add Android setup command

---
+++ fish/functions/airfox-android-setup.fish
@@ -0,0 +1,4 @@
+function airfox-android-setup
+  cd ~/s/a/airfox-mobile-apps
+  vim apps-lending/src/development/assets/master-build-config.json apps-lending/src/development/assets/build-config-development.json
+end
