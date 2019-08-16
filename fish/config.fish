09934b10fa72de0fc0fba8f8aa5454071fba2447
Ken Kan <kenhkan@gmail.com>
2019-08-15 21:29:30 -0400

    Add fisher installation

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,15 @@ setenv EDITOR vim
 setenv KUBE_EDITOR vim
 
 
+# Fisher
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+
 # iTerm integration
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
