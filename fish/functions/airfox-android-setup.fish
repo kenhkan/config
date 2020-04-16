cb5f0799a0a5c2f96fd46375f76c56b1d3f331b1
Ken Kan <kenhkan@gmail.com>
2020-04-16 10:06:58 -0400

    Modify default settings

--- fish/functions/airfox-android-setup.fish
+++ fish/functions/airfox-android-setup.fish
@@ -1,4 +1,10 @@
 function airfox-android-setup
   cd ~/s/a/airfox-mobile-apps
+  set TEMP_PATH __temp
+  set CONFIG_PATH apps-lending/src/main/assets/build-config-production.json
+  jq '.debugLogging = true | .emailEncrytpKey = "g8SregADEuGnw9JckWgf2zXYDTay2pwE"' $CONFIG_PATH > $TEMP_PATH
+  cat $TEMP_PATH > $CONFIG_PATH
+  rm $TEMP_PATH 
+  sed -i -e 's/^kapt.incremental.apt = true/kapt.incremental.apt = false/g' gradle.properties
   vim apps-lending/src/development/assets/master-build-config.json apps-lending/src/development/assets/build-config-development.json
 end
