a6ed2bd350f52177288c1537de9542fe800e96b5
Ken Kan <kenhkan@gmail.com>
2020-04-17 20:38:52 -0400

    Fix android setup script

--- fish/functions/airfox-android-setup.fish
+++ fish/functions/airfox-android-setup.fish
@@ -1,10 +1,15 @@
 function airfox-android-setup
   cd ~/s/a/airfox-mobile-apps
   set TEMP_PATH __temp
+
   set CONFIG_PATH apps-lending/src/main/assets/build-config-production.json
   jq '.debugLogging = true | .emailEncrytpKey = "g8SregADEuGnw9JckWgf2zXYDTay2pwE"' $CONFIG_PATH > $TEMP_PATH
   cat $TEMP_PATH > $CONFIG_PATH
+
+  cat gradle.properties | sed -e 's/^kapt.incremental.apt = true/kapt.incremental.apt = false/g' > $TEMP_PATH 
+  cat $TEMP_PATH > gradle.properties
+
   rm $TEMP_PATH 
-  sed -i -e 's/^kapt.incremental.apt = true/kapt.incremental.apt = false/g' gradle.properties
+
   vim apps-lending/src/development/assets/master-build-config.json apps-lending/src/development/assets/build-config-development.json
 end
