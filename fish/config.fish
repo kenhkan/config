e1fd543e926b41a3f67904ebd7e153929c519abc
Ken Kan <kenhkan@gmail.com>
2020-06-25 18:23:32 -0400

    Add rust to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH $HOME/.config/bin $HOME/.local/bin $HOME/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /Applications/Postgres.app/Contents/Versions/latest/bin
+set PATH $HOME/.cargo/bin $HOME/.config/bin $HOME/.local/bin $HOME/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /Applications/Postgres.app/Contents/Versions/latest/bin
 setenv EDITOR vim
 setenv KUBE_EDITOR vim
 
